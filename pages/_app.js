88b4e61df3983072416cc0ff044521d729fc65fb
FrekiManagarm <mathieu.chambaud@ynov.com>
2022-08-09 17:35:58 +0200

    push

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
-import '../styles/globals.css'
 import { NextUIProvider } from '@nextui-org/react';
+import { darkTheme, lightTheme } from '../styles/theme';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <NextThemesProvider
+      defaultTheme='system'
+      attribute='class'
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className
+      }}
+    >
+      <NextUIProvider theme={darkTheme}>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
   )
 }
 
