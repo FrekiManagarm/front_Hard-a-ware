33b28a107db8df676f3c9aaf846d776ab2bafb61
Mathieu Chambaud <mathchambaud@icloud.fr>
2022-09-21 09:45:56 +0200

    testing links to amazon for products

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Image from "next/image"
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import { MAIN_COLOR } from "../settings/constants"
 
 export default function Home() {
   return (
-    <>
-        
-    </>  
+    <div style={{
+      alignItems: 'center',
+      padding: "5rem"
+    }}>
+        <a target="_blank" href="https://www.amazon.fr/ASUS-TUF-B450-PLUS-Gaming-II/dp/B08K93G7Q9?ref_=Oct_d_obs_d_430341031&amp;pd_rd_w=cjScV&amp;content-id=amzn1.sym.841e11e3-55d4-4422-a11a-1f6d3371ceeb&amp;pf_rd_p=841e11e3-55d4-4422-a11a-1f6d3371ceeb&amp;pf_rd_r=MR7F1NFGWGEH8VJJ3Q13&amp;pd_rd_wg=gW1iY&amp;pd_rd_r=c81fc516-2ddb-4bf0-9a64-1b6bafa63b7c&amp;pd_rd_i=B08K93G7Q9&_encoding=UTF8&tag=frekimanagarm-21&linkCode=ur2&linkId=b0e96329fd12cf4b9af8517f0e9e517e&camp=1642&creative=6746">ASUS TUF B450-PLUS Gaming II</a>
+    </div>  
   )
 }
