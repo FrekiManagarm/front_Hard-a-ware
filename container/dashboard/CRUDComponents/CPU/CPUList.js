a28b471be5797c6c0f583b60099bc4c6a24c1af4
Mathieu Chambaud <mathieu.chambaud@niji.fr>
2022-09-29 17:50:54 +0200

    push

--- container/dashboard/CRUDComponents/CPU/CPUList.js
+++ container/dashboard/CRUDComponents/CPU/CPUList.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useEffect, useState, useRef } from 'react'
+import GetAPIData from '../../../../helpers/get_api_data';
+import { useFetchSwr } from '../../../../hooks/useFetchSwr';
 
 const CPUList = () => {
+
+  const mounted = useRef();
+
+  const { data } = useFetchSwr(`${process.env.SERVER_API}/api/CPUs`);
+  console.log(data, 'data api')
+
   return (
-    <div>CPUList</div>
+    <div ref={mounted} >CPUList</div>
   )
 }
 
