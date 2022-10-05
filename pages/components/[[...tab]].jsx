618cc68a56297e16153a5ddf912e505b9c75eff7
Mathieu Chambaud <mathieu.chambaud@niji.fr>
2022-10-05 17:39:02 +0200

    push

--- pages/components/[[...tab]].jsx
+++ pages/components/[[...tab]].jsx
@@ -1,6 +1,39 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import Error from 'next/error';
+
+const mockData = [];
 
 const ComponentsPage = () => {
+
+  const router = useRouter()
+  const { query } = router;
+
+  const displayComponent = () => {
+    if (query.tab && query.tab[0] && !query.tab[1]) {
+      switch (query.tab[0]) {
+        case "cpu":
+          return;
+        case "gpu":
+          return;
+        case "case":
+          return;
+        case "psu":
+          return;
+        case "ram":
+          return;
+        case "motherboard":
+          return;
+        case "cooling":
+          return;
+        case "":
+          return;
+        case "":
+          return;
+        default:
+          return <Error />;
+      }
+    }
+  }
 
   return (
     <>
