7085dd7c0d793f974c26df656940bd4cc50a6ffa
FrekiManagarm <mathieu.chambaud@ynov.com>
2022-10-13 16:26:00 +0200

    push

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,9 +10,10 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                  <link rel="preconnect" href="https://fonts.googleapis.com" /> 
-                  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' /> 
-                  {/* <link href="https://fonts.googleapis.com/css2?family=Baloo+Da+2:wght@400;500;600;700;800&display=swap" rel="stylesheet" /> */}
+                    <link rel="shortcut icon" href="/Hard-A-ware_logo.png"/> 
+                    <link rel="preconnect" href="https://fonts.googleapis.com" /> 
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+                    {/* <link href="https://fonts.googleapis.com/css2?family=Baloo+Da+2:wght@400;500;600;700;800&display=swap" rel="stylesheet" /> */}
                 </Head>
                 <body>
                     <Main />
